2163e9ad434bd144873963e3dd7de956262e0c0a
cjus <Carlos.Justiniano@gmail.com>
2022-08-27 17:38:46 -0600

    support for arm64

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 # Syntax: ./build.sh
 # Use --no-cache=true  when necessary
 VERSION_TAG=$(<VERSION)
-docker build -t hydra-logging-service:$VERSION_TAG .
+SERVICE_HOST=pnxtech
+SERVICE_NAME=hydra-logging-svcs
+docker buildx build --platform=linux/amd64,linux/arm64 --push --no-cache -t $SERVICE_HOST/$SERVICE_NAME:$VERSION_TAG .
